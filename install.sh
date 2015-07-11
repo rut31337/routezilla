2b9fe217b0647c7b37d27ae6806cd25f79217a7b
Patrick T. Rutledge III <rut31337@users.noreply.github.com>
2015-07-11 10:13:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # install docker, useful utilities, and quagga image
 
 dnf -y install git docker bridge-utils telnet
+systemctl enable docker
+systemctl start docker
 docker pull abaranov/quagga
 bash -c "curl https://raw.githubusercontent.com/jpetazzo/pipework/master/pipework" > ~/pipework
 chmod +x ~/pipework
